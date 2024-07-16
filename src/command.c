cba79a127de28f96d9427a01b1b78b941ea53de3
Limule-Cams <Limule-Cams@users.noreply.github.com>
2024-07-17 00:31:00 +0100

    add parser

--- src/command.c
+++ src/command.c
@@ -9,6 +9,7 @@ Bool cmd_null(Command_p cmd){
 
     if(cmd==NULL)
         return true ;
+
     return false;
 }
 
@@ -21,12 +22,25 @@ void remove_last(Command_p cmd){
         prev = elm;
         elm = elm->next;
     }
-    prev->next = NULL
+    prev->next = NULL;
     free(elm);
     elm = NULL;
 }
 
-Command_p add_cmd(Command_p cmd){
+
+
+int count_cmd(Command_p cmd){
+    int nbre = 0;
+    while(cmd!= NULL){
+        nbre++;
+        cmd = cmd->next;
+    }
+    return nbre;
+}
+
+
+
+Command_p add_cmd(Command_p cmd, char *buff){
 
     Command_elm *elm;
     if(elm = malloc(sizeof(*elm))==NULL){
@@ -35,17 +49,37 @@ Command_p add_cmd(Command_p cmd){
     }
 
     /*
-    init membre of struct
-    use strtok for write in elm->arg, elm->sub_arg, 
-    elm->nbre for count the nbre elm in our list of command +=1 
+    parser l'entrée qui est la chaine buff
+    on utilise strtok pour diviser son contenu en une commande elm->cmd
+    ses argument elm->args
+    et on compte son nbre d'argument avec elm->nb_arg
     */
+
+    char delim[]=" ";
+    char *token = strtok(buff, delim);
+    if(token!=NULL)
+        elm->cmd = strdup(token);
+    else{
+        free(elm);
+        perror("parse de commande erreur");
+        exit(EXIT_FAILURE);
+    }
+    elm->nb_arg = 0;
+    while ((token = strtok(NULL, delim))!=NULL)
+    {
+        elm->args[elm->nb_arg] = strdup(token);
+        elm->nb_arg++;
+    }
+    
 
 
     if(cmd_null(cmd)){
         elm->next = cmd;
         return elm;
     }else{
-        if(cmd->nbre==CMD_MAX){
+
+        int count_elm = count_cmd(cmd);
+        if(count_elm==CMD_MAX){
 
             remove_last(cmd);
             elm->next = cmd;
@@ -57,3 +91,5 @@ Command_p add_cmd(Command_p cmd){
         }
     }
 }
+
+
