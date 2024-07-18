227f3bcfe35f6f30765d1eec0b9c67bf8442bb8a
Limule-Cams <hcamara903@gmail.Com>
2024-07-18 03:26:32 +0100

    fix bug parser

--- src/main.c
+++ src/main.c
@@ -1,23 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "../include/shell.h"
+#include "../include/command.h"
 
 
 
 
 int main(){
 
-	char cmd[LEN_STDIN], *token;
+	char buff[LEN_STDIN];
+	Command_elm * cmd = _init_command();
+	Command_elm * cmd_shell = _init_command();
 
 	while(1){
 
-		if(fgets(cmd, LEN_STDIN, stdin)!=NULL){
+		printf("~");
+		if(fgets(buff, LEN_STDIN, stdin)!=NULL){
 
-			printf("~");
+			buff[LEN_STDIN - 1] = '\0';
 
-			cmd[LEN_STDIN - 1] = '\0';
-		   
+			cmd_shell = add_cmd(cmd, buff);
+			if(cmd_shell->cmd == NULL){
+				perror("erreur dans la fonction add_cmd");
+			}
 
+
+			if(strcmp(cmd_shell->cmd, "exit")==0){  
+				memfree(cmd_shell);
+				fprintf(stdout, "vous quittez le programme");
+				break;
+			}
+
+			if(strcmp(cmd_shell->cmd, "history")==0) print_current_history(cmd_shell);
+			cmd = cmd_shell;
 
 		}else{
 			fprintf(stdin, "Erreur de lecture de la commande");
